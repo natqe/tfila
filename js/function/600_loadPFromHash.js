dfbbfa61e372c6fc28c40f326302962d6123d182
natqe <natqe3@gmail.com>
2018-01-02 14:56:13 +0200

    fix loadfromhash - push oldParas

--- js/function/600_loadPFromHash.js
+++ js/function/600_loadPFromHash.js
@@ -6,12 +6,12 @@ function loadPFromHash() {
 
         const p = location.hash ? sele(sele(decodeURI(location.hash)), '[id]') : false;
         if (p && !isNaN(p.id)) {
+            oldParas.push({ id: p.id, HTML: p.innerHTML });
             const dataPara = paras.find(para => para.id === p.id);
             if (dataPara) {
                 p.innerHTML = dataPara.HTML;
             } else {
                 get('p_from', p.id).then(data => {
-                    oldParas.push({ id: p.id, HTML: p.innerHTML });
                     paras.push({ id: p.id, HTML: data });
                     p.innerHTML = data;
                     p.scrollIntoView();
